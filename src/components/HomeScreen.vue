301afdc2c38af8409c47f76ec92a24618d543c60
Giovanni <giovanni.wifc@gmail.com>
2023-10-06 11:02:45 -0300

    Remoção das telas padrões

---
+++ src/components/HomeScreen.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h3>
+      Projeto Funcionando
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+
+h3 {
+  font-size: 1.2rem;
+  color: green;
+}
+
+</style>
